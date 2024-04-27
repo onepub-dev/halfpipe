0bb951a7c60e07d20b4db634b62c54b1bcc7ab3f
Brett Sutton <bsutton@noojee.com.au>
2024-04-27 22:32:43 +1000

    doco.

--- lib/src/pipeline/pipe_section.dart
+++ lib/src/pipeline/pipe_section.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_returning_this
+// ignore_for_file: avoid_returning_this, comment_references
 
 import 'dart:async';
 
@@ -12,8 +12,8 @@ abstract class PipeSection<I, O> {
   /// and passing data out to the next [PipeSection].
   /// [srcIn] is a stream from the prior [PipeSection] equivalent stdout.
   /// [srcErr] is a stream from the prior [PipeSection] equivalent to stderr.
-  /// [sinkOut] is where this [PipeSection] writes it's 'good' data.
-  /// [sinkErr] is where this [PipeSection] writes its 'bad'data.
+  /// [outController] is where this [PipeSection] writes it's 'good' data.
+  /// [errController] is where this [PipeSection] writes its 'bad'data.
   /// A Future is returned that completes when this [PipeSection] has completed
   /// generating/processing the [srcIn] and [srcErr] streams.
   Future<void> start(Stream<I> srcIn, Stream<I> srcErr);
