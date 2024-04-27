89f272e2495864808004c83b0579ca4726b3dd7b
Brett Sutton <bsutton@noojee.com.au>
2024-04-27 22:33:20 +1000

    merge the two test classes into one.

--- test/src/half_pipe_test.dart
+++ test/src/half_pipe_test.dart
@@ -119,4 +119,29 @@ void main() {
       });
     });
   });
+
+  group('A group of tests', () {
+    test('First Test', () async {
+      await HalfPipe().command('ls').print();
+
+      await HalfPipe().commandAndArgs('ls', args: ['-la']).print();
+      await HalfPipe().command('ls -la').print();
+      await HalfPipe().command('ls -la').printerr();
+      await HalfPipe().command('ls -la').printmix();
+      await HalfPipe().command('cat image.png').run();
+      await HalfPipe().command('ls -la').print();
+      await HalfPipe().command('ls -la').printmix();
+
+      await HalfPipe().command('ls -la').print();
+      await HalfPipe().command('ls -la').exitCode();
+
+      await HalfPipe()
+          .commandAndArgs('ls',
+              args: ['-la'],
+              runInShell: true,
+              terminal: true,
+              extensionSearch: false)
+          .print();
+    });
+  });
 }
