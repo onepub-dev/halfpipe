ae2466c2ff74d672a7861eed475b0ad50efec4ec
Brett Sutton <bsutton@onepub.dev>
2024-05-03 21:49:37 +1000

    Added a wrapper for stdin so that we can run multiple pipelines that subscribe to stdin. Normally a second subscription attempt would fail.

---
+++ lib/src/pipeline/stdin_wrapper.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'dart:io';
+
+import '../util/stream_controller_ex.dart';
+
+/// Runs [callback] with access to [stdin] for the duration of the
+/// call.
+Future<void> withStdin(
+    Future<void> Function(StreamControllerEx<List<int>>) callback,
+    {String? debugName}) async {
+  final wrapper = StdinWrapper();
+  final controller =
+      StreamControllerEx<List<int>>(debugName: debugName ?? 'wrapped stdin');
+
+  wrapper.stdinControllers.add(controller);
+  await callback(controller);
+
+  wrapper.stdinControllers.remove(controller);
+}
+
+/// Stdin can only be listened to once but we allow users to create
+/// multiple pipelines and each needs the ability to dip into
+/// stdin at the point in time the run.
+/// As stdin is a single stream this won't work as it can only be listended to
+/// once.
+/// This wrapper allows multiple listeners to subscribe to stdin and distributes
+/// the events to all listeners.
+///
+class StdinWrapper {
+  factory StdinWrapper() => _stdinWrapper;
+
+  StdinWrapper._internal() {
+    /// distribute stdin events to all active controllers.
+    stdin.listen((event) {
+      for (final controller in stdinControllers) {
+        controller.sink.add(event);
+      }
+    });
+  }
+
+  final stdinControllers = <StreamControllerEx<List<int>>>[];
+  static final StdinWrapper _stdinWrapper = StdinWrapper._internal();
+}
