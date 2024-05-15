509655f6e3470828f51fdd48e96973085042b66d
Brett Sutton <bsutton@onepub.dev>
2024-05-15 21:34:37 +1000

    fixed unit terts for non-zero exit code.

--- test/src/pipeline/command_pipe_section_test.dart
+++ test/src/pipeline/command_pipe_section_test.dart
@@ -1,19 +1,11 @@
-import 'package:dcli_core/dcli_core.dart';
+import 'package:dcli_core/dcli_core.dart' hide RunException;
 import 'package:halfpipe/halfpipe.dart';
+import 'package:halfpipe/src/run_exception.dart';
 import 'package:path/path.dart' hide equals;
 import 'package:test/test.dart';
 
-String buildTestAppCommand(
-    {int exitCode = 0, int outLines = 0, int errLines = 0}) {
-  final sb = StringBuffer()
-    ..write('dart ')
-    ..write(join(pwd, 'test', 'src', 'test_app.dart'))
-    ..write(' --exit-code $exitCode')
-    ..write(' --stdout-lines $outLines')
-    ..write(' --stderr-lines $errLines');
+import '../test_app.dart';
 
-  return sb.toString();
-}
 
 void main() {
   /// Test that the [CommandPipeSection] completes when the command completes
@@ -33,12 +25,22 @@ void main() {
 
   test('CommandPipeSection completes when command exits with non-0', () async {
     await withTempDirAsync((tempDir) async {
-      final exitCode = await HalfPipe()
-          .command(buildTestAppCommand(exitCode: 1), workingDirectory: tempDir)
-          .exitCode();
-
-      // Assertions
-      expect(exitCode, 1, reason: 'Command should exit with 1');
+      final commandLine = buildTestAppCommand(exitCode: 1);
+      var threw = false;
+      try {
+        await HalfPipe()
+            .command(commandLine, workingDirectory: tempDir)
+            .exitCode();
+      } on RunException catch (e) {
+        expect(e.exitCode, equals(1));
+        expect(e.cmdLine, equals(commandLine));
+        expect(
+            e.reason, equals('The command exited with a non-zero exit code.'));
+        threw = true;
+      }
+      if (threw == false) {
+        fail('Expected an exception to be thrown');
+      }
     });
   });
 }
