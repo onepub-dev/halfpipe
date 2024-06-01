c4a3cd7ffbb6c521926fd61b862e98c18d1bf07a
Brett Sutton <bsutton@onepub.dev>
2024-06-02 00:09:33 +1000

    fixed bug in readfile. It wasn't reading the errController sink incorrectly and failed to cancel the read subscription.

--- lib/src/processors/read_file.dart
+++ lib/src/processors/read_file.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:completer_ex/completer_ex.dart';
+import 'package:logging/logging.dart';
 
 import '../util/stream_controller_ex.dart';
 import 'processor.dart';
@@ -9,6 +10,8 @@ import 'processor.dart';
 class ReadFile extends Processor<List<int>, List<int>> {
   ReadFile(this.pathToFile);
   String pathToFile;
+
+  final log = Logger((ReadFile).toString());
 
   late final _done = CompleterEx<void>(debugName: 'ReadFile');
 
@@ -21,21 +24,27 @@ class ReadFile extends Processor<List<int>, List<int>> {
     StreamControllerEx<List<int>> srcErr,
   ) async {
     try {
+      srcErr.stream.listen(errController.sink.add);
+
       // Read the file as a list of strings
       final fileStream = File(pathToFile).openRead();
+      log.fine(() => 'opened $pathToFile');
+
+      late StreamSubscription<List<int>> sub;
 
       /// write the contents of the file into the stream.
-      fileStream.listen((event) {
+      sub = fileStream.listen((event) {
         outController.sink.add(event);
       })
         ..onDone(() {
+          log.fine(() => 'ReadFile: onDone');
           if (!_done.isCompleted) {
             _done.complete();
           }
+          sub.cancel();
+          log.fine(() => 'ReadFile: sub cancelled');
         })
         ..onError(_done.completeError);
-
-      await errController.sink.addStream(srcErr.stream);
     }
     // ignore: avoid_catches_without_on_clauses
     catch (e) {
